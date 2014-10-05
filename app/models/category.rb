75342d731996bce6de916805d53be53786400d62
Corey Condardo <ccondardo@gmail.com>
2014-10-05 00:32:51 -0400

    recent videos

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ActiveRecord::Base
   has_many :videos, through: :video_categories
 
   validates_presence_of :name
+
+  def self.recent_videos(category)
+    category.videos.limit(6).order("created_at DESC")
+  end
 end
