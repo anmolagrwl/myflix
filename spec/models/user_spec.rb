fc8f604ea6604d7280547c7957d64e8737f8fef3
Corey Condardo <ccondardo@gmail.com>
2014-10-11 12:47:33 -0400

    fixing rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,32 +1,9 @@
 require 'spec_helper'
 
-
-
 describe User do
-  it { should validate_presence_of(:email)}
-  it { should validate_presence_of(:full_name)}
-  it { should validate_presence_of(:password)}
-
-  describe "Validations" do
-    
-    it "should only require a password, but no password_confirmation" do
-      user = User.create(password: "testing123", email: "test@test.com", full_name: "david lee")
-      user.should be_valid
-    end
-
-    it "should not be valid without a password" do
-      user = User.create(password: nil, email: "test@test.com", full_name: "david lee")
-      user.should_not be_valid
-    end
-
-    it "should not be valid with an empty string password" do
-      user = User.create(password: "", email: "test@test.com", full_name: "david lee")
-      user.should_not be_valid
-    end
-
-    it "should not be valid with a password less than 6 characters" do
-      user = User.create(password: "aaaaa", email: "test@test.com", full_name: "david lee")
-      user.should_not be_valid
-    end
-  end
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_presence_of(:full_name) }
+  it { should have_secure_password }
+  it { should ensure_length_of(:password).is_at_least(6) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
+describe User do
+  it { should validate_presence_of(:email)}
+  it { should validate_uniqueness_of(:email)}
+  it { should validate_presence_of(:full_name)}
+  it { should validate_presence_of(:password)}
+
+  describe "Validations" do
+    
+    it "should only require a password, but no password_confirmation" do
+      user = User.create(password: "testing123", email: "test@test.com", full_name: "david lee")
+      user.should be_valid
+    end
+
+    it "should not be valid with an empty string password" do
+      user = User.create(password: "", email: "test@test.com", full_name: "david lee")
+      user.should_not be_valid
+    end
+
+    it "should not be valid with a password less than 6 characters" do
+      user = User.create(password: "aaaaa", email: "test@test.com", full_name: "david lee")
+      user.should_not be_valid
+    end
+  end
+end
+
 describe Video do
   it { should have_many(:categories)}
   it { should validate_presence_of(:title)}
