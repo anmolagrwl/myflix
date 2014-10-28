452a468358093082b26e0758f1458f2a2697b6ad
Corey Condardo <ccondardo@gmail.com>
2014-10-27 23:16:47 -0400

    adding ratings to queue

--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -52,4 +52,30 @@ describe QueueItem do
       expect(queue_item.rating).to eq(nil)
     end
   end
+
+  describe "#rating=" do
+    it "changes the rating of an existing review" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, user: user, video: video, rating: 2)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = 4
+      expect(Review.first.rating).to eq(4)
+    end
+    it "clears the rating of an existing review" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, user: user, video: video, rating: 2)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = nil
+      expect(Review.first.rating).to be_nil
+    end
+    it "creates a review with the rating if no review exists" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = 3
+      expect(Review.first.rating).to eq(3)
+    end
+  end
 end
