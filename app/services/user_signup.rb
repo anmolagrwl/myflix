6b6e430184d2cf8c349edb1594dfb5ab39a11132
Corey Condardo <ccondardo@gmail.com>
2014-12-24 02:01:35 -0500

    refactoring long methods and cleaning up some misc stuff

--- app/services/user_signup.rb
+++ app/services/user_signup.rb
@@ -1,23 +1,19 @@
 class UserSignup
   
   attr_reader :error_message
+  attr_reader :user
 
   def initialize(user)
     @user = user
+    @status = nil
+    @error_message = nil
   end
   
   def sign_up(stripe_token, invitation_token)
     if @user.valid?
-      Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-      charge = StripeWrapper::Charge.create(
-        :amount => 999,
-        :card => stripe_token,
-        :description => "Payment for #{@user.email}"
-      )
+      charge = attempt_charge(stripe_token, @user)
       if charge.successful?
-        @user.save
-        handle_invitation(invitation_token)
-        AppMailer.send_welcome_email(@user).deliver       
+        process_user_creation(@user, invitation_token)     
         @status = :success
         self
       else
@@ -43,5 +39,20 @@ class UserSignup
       invitation.inviter.follow(@user)
       invitation.update_column(:token, nil)
     end
+  end
+
+  def process_user_creation(user, invitation_token)
+    @user.save
+    handle_invitation(invitation_token)
+    AppMailer.send_welcome_email(user).deliver       
+  end
+
+  def attempt_charge(stripe_token, user)
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+    StripeWrapper::Charge.create(
+      :amount => 999,
+      :card => stripe_token,
+      :description => "Payment for #{user.email}"
+    )
   end
 end
