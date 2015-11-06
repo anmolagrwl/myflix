01afc8ef90da2c4d4c08b5058c24e2ecc97a491d
Corey Condardo <ccondardo@gmail.com>
2015-11-06 10:07:26 -0500

    Breaking carrierwave so i can deploy

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AWS_ACCESS'],
-      :aws_secret_access_key  => ENV['AWS_SECRET'],
-  }
-  if Rails.env.staging? || Rails.env.production?    
-    config.fog_directory  = 'coreycodes-myflix'
-  else
-    config.fog_directory  = 'coreycodes-myflix-development'
-  end
+  # config.storage = :fog
+  # config.fog_credentials = {
+  #     :provider               => 'AWS',
+  #     :aws_access_key_id      => ENV['AWS_ACCESS'],
+  #     :aws_secret_access_key  => ENV['AWS_SECRET'],
+  # }
+  # if Rails.env.staging? || Rails.env.production?
+  #   config.fog_directory  = 'coreycodes-myflix'
+  # else
+  #   config.fog_directory  = 'coreycodes-myflix-development'
+  # end
 end
